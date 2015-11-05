14d4ec508810a6f6bc4e24bafc419176f717b4af
Marcos Baez <mbaezpy@gmail.com>
2015-11-05 08:40:15 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+/* your code should go in this file */
+
+/* 
+ * The data is available in the array *data*
+ * Each element of the array has the following structure:
+ *  {
+ *    word_en : "Apple",  // word in english
+ *    word_de : "Apfel"   // word in german
+ *  }
+ */ 
+
+var tmpl = ' <li id="ID">' +
+           '  <h3>WORD</h3>' +
+           '  <h3 class="solution">SOLUTION</h3>'+
+           ' </li> ';
+
+$(document).ready(function(){
+
+
+});
+
+
+
+
+
+
+
---
+++ data.js
@@ -0,0 +1,32 @@
+/* Don't modify this file */
+
+var data = [
+  {
+    word_en : "man",
+    word_de : "Mann"
+  },
+  {
+    word_en : "yes",
+    word_de : "ja"
+  },
+  {
+    word_en : "hello",
+    word_de : "hallo"
+  },  
+  {
+    word_en : "house",
+    word_de : "Haus"
+  },
+  {
+    word_en : "red",
+    word_de : "rot"
+  },
+  {
+    word_en : "Love",
+    word_de : "Liebe"
+  },
+  {
+    word_en : "good",
+    word_de : "gut"
+  }   
+];
